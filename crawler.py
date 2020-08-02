963dd002f904780bef6e2e421f4790e42717df77
tweichuan <wctsai20002@gmail.com>
2020-08-02 15:15:17 +0800

    Modify os.system to subprocess

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import os
 import html
 import requests
+import subprocess
 import urllib.parse
 from dotenv import load_dotenv, find_dotenv
 from django.core.validators import URLValidator
@@ -32,12 +33,12 @@ def load_txt_data(use_keyword, use_url):
     urls = []
     if use_keyword:
         with open("./keywords.txt", "r", encoding="utf-8") as f:
-            keywords = [line.rstrip("\n") for line in f.readlines()]
+            keywords = [line.strip() for line in f.readlines()]
     if use_url:
         with open("./urls.txt", "r", encoding="utf-8") as f:
             url_validate = URLValidator()
             for line in f.readlines():
-                line = line.rstrip("\n")
+                line = line.strip()
                 try:
                     url_validate(line)
                     urls.append(line)
@@ -76,15 +77,15 @@ def download_keyword_data(base_url, keywords, maximum_page, user_agent):
             search_url = base_url + search_parameter
 
             # wget google index page
-            wget_download(search_url, index_page_path, user_agent, False)
+            wget_download("keywords", search_url, index_page_path, user_agent, False)
             new_index_page_path = rename_html_file(index_page_path, "index_page.html")
 
             # wget result
-            download_and_replace_result(page_path, new_index_page_path)
+            download_and_replace_result(page_path, new_index_page_path, user_agent)
 
         replace_page_number_href(keyword_path, maximum_page)
 
-def download_and_replace_result(page_path, index_page_path):
+def download_and_replace_result(page_path, index_page_path, user_agent):
     html_code = ""
     with open(index_page_path, "r+", encoding="utf-8") as f:
         html_code = f.read()
@@ -97,7 +98,7 @@ def download_and_replace_result(page_path, index_page_path):
 
         # make result dir
         for i, href in enumerate(hrefs):
-            vaild_filename = get_valid_filename(href)[:20]
+            vaild_filename = get_valid_filename(href)[:30]
             result_path = page_path.rstrip("/") + "/" + vaild_filename
             if not os.path.isdir(result_path):
                 os.mkdir(result_path)
@@ -106,7 +107,7 @@ def download_and_replace_result(page_path, index_page_path):
                 continue
 
             # wget result
-            wget_download(href, result_path)
+            wget_download("keywords", href, result_path, user_agent)
             new_result_path = rename_html_file(result_path, "result_" + str(i + 1) + ".html")
             print("new_result_path : ", new_result_path)
 
@@ -124,6 +125,17 @@ def download_and_replace_result(page_path, index_page_path):
         f.seek(0)
         f.write(html_code)
         f.truncate()
+
+def download_url_data(urls, user_agent):
+    for url in urls:
+        vaild_filename = get_valid_filename(url)[:30]
+        url_path = ".download_data/urls/" + vaild_filename
+        if not os.path.isdir(url_path):
+            os.mkdir(url_path)
+        else:
+            print("Duplicate url of : ", url)
+            continue
+        wget_download("urls", url, url_path, user_agent)
 
 def rename_html_file(path, file_name):
     # find .orig to know name of html
@@ -186,7 +198,7 @@ def replace_page_number_href(path, maximum_page):
             f.write(html_code)
             f.truncate()
 
-def wget_download(url, path, user_agent, download_raw = True):
+def wget_download(species, url, path, user_agent, download_raw = True):
     # sometimes wget will failed, so backup by requests
     if download_raw:
         download_raw_html(url, path, user_agent)
@@ -196,7 +208,10 @@ def wget_download(url, path, user_agent, download_raw = True):
     local_path = "-P " + path
     wget_command = "wget -p -E -k -K -H -nH -e robots=off "
     wget_command = wget_command + local_path + user_agent + url
-    os.system(wget_command)
+    output_bytes = subprocess.check_output(wget_command, shell=True, stderr=subprocess.STDOUT)
+    log_path = "./download_data/" + species + "/download_log"
+    with open(log_path, "a", encoding="utf-8") as f:
+        f.write(output_bytes.decode("utf-8"))
 
 # use requests to download html
 def download_raw_html(url, path, user_agent):
@@ -213,6 +228,7 @@ def Crawler():
     print(env_map)
     keywords, urls = load_txt_data(env_map["USE_KEYWORD"], env_map["USE_URL"])
     download_keyword_data(env_map["BASE_URL"], keywords, env_map["MAXIMUM_PAGE"], env_map["USER_AGENT"])
+    download_url_data(urls, env_map["USER_AGENT"])
     
 
 if __name__ == "__main__":
